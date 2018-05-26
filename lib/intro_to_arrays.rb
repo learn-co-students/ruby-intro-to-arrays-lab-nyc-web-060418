6d349e88cc81646ca9d5edd54403844a844e4095
Giles Bonner <gjbonner@gmail.com>
2018-05-26 23:19:35 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,41 @@
+def first_element(array)
+  return array[0]
+end
+
+def instantiate_new_array
+  Array.new
+end
+  
+def array_with_two_elements
+  array = [1,2]
+  return array
+end
+  
+
+def first_element(array)
+  return array[0]
+end
+    
+def third_element(array)
+  return array[2]
+end
+
+def first_element_with_array_methods(array)
+  return array.shift
+end
+
+
+
+def last_element_with_array_methods(array)
+  return array.last
+end
+  
+def length_of_array(array)
+  return array.length
+end
+
+def last_element(array)
+  return array.last
+end
+
+
